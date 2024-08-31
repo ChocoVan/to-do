a41251f63d9228d1e7fade97b349f59272eef7da
ChocoVan <128563886+ChocoVan@users.noreply.github.com>
2024-08-31 00:20:09 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,3 @@
-/*// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyC4iYLMXTOQNjb6r-dxhG0qTh2skbnJ6Fs",
-  authDomain: "to-do-a3ed7.firebaseapp.com",
-  projectId: "to-do-a3ed7",
-  storageBucket: "to-do-a3ed7.appspot.com",
-  messagingSenderId: "140118077212",
-  appId: "1:140118077212:web:c128a7f129fb36ec8394d9"
-};*/
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
